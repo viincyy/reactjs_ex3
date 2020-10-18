4c8e8605d78288e8cf170dfa9c33c3cd14b7fbee
viincyy <viincyy126@gmail.com>
2020-10-18 10:21:08 +0700

    ng-template

---
+++ angular/src/app/directive/directive-switch/directive-switch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directive-switch',
+  templateUrl: './directive-switch.component.html',
+  styleUrls: ['./directive-switch.component.css'],
+})
+export class DirectiveSwitchComponent implements OnInit {
+  color: string = 'red';
+  constructor() {}
+  ChangeColor(color: string): void {
+    this.color = color;
+  }
+  ngOnInit(): void {}
+}
--- angular/src/app/directive/directive.module.ts
+++ angular/src/app/directive/directive.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DirectiveIfComponent } from './directive-if/directive-if.component';
 import { DirectiveIfElseComponent } from './directive-if-else/directive-if-else.component';
+import { DirectiveSwitchComponent } from './directive-switch/directive-switch.component';
 
 @NgModule({
-  declarations: [DirectiveIfComponent, DirectiveIfElseComponent],
+  declarations: [
+    DirectiveIfComponent,
+    DirectiveIfElseComponent,
+    DirectiveSwitchComponent,
+  ],
   imports: [CommonModule],
-  exports: [DirectiveIfComponent, DirectiveIfElseComponent],
+  exports: [
+    DirectiveIfComponent,
+    DirectiveIfElseComponent,
+    DirectiveSwitchComponent,
+  ],
 })
 export class DirectiveModule {}
