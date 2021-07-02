babd62db1e5141cba3ffd2b365cc066cb74f161d
viincyy <viincyy126@gmail.com>
2021-07-02 18:53:16 +0700

    default parameter value && enhance object literals

---
+++ basic javascript/default-parameter-value.js
@@ -0,0 +1,8 @@
+function log(log = 'Default Parameter Value'){
+    console.log(log);
+}
+
+function log2(log, type='log'){
+    console[type](log);
+}
+log2('Messge', error);
---
+++ basic javascript/enhanced-object-literal.js
@@ -0,0 +1,23 @@
+/*  
+    1. Định nghĩa key: value cho object
+    2. Định nghĩa method cho Object
+    3. Định nghĩa key cho object dưới dạng biến
+*/
+
+var _name = 'JS';
+var price = 1000;
+var course = {
+    _name, 
+    price, 
+    getName(){
+        return _name;
+    }
+}
+
+// 3
+var fieldName = 'name';
+var fieldPrice = 'price';
+var course = {
+    [fieldName]: 'JS', 
+    [fieldPrice]: 1000
+}
